953ddad0d8ae0d8da09e49a763562098e978b619
sxs555 <shixuesong1988@126.com>
2016-03-25 15:45:53 +0800

    333

--- app/src/main/java/com/example/thinkpad/android_45_githubdemo/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/android_45_githubdemo/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //第一次
+    //第二次，哈哈
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
