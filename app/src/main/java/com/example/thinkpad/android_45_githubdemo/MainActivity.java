19a08d761006a6d7e9e6a917edc91b79e7d11a8e
sxs555 <shixuesong1988@126.com>
2016-03-25 15:24:46 +0800

    hgjkjkl

---
+++ app/src/main/java/com/example/thinkpad/android_45_githubdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thinkpad.android_45_githubdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //第一次
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
